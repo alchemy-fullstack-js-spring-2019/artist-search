7e55266658d41e239abbc5484dc3517cfebf6ea6
UncleJerry <jared.myhrberg@gmail.com>
2019-05-21 17:41:00 -0700

    paging done

--- src/containers/ArtistView.js
+++ src/containers/ArtistView.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import AlbumList from '../components/artist/AlbumList';
 import albumsGetter from '../services/albumsGetter';
 import PropTypes from 'prop-types';
+import Paging from '../components/Paging';
 
 export default class ArtistView extends Component {
     
   state = {
-    albums: null
+    albums: null,
+    currentPage: 0,
+    totalPages: 1
   };
 
   static propTypes = {
@@ -19,8 +22,25 @@ export default class ArtistView extends Component {
   }
 
   componentDidMount() {
-    return albumsGetter(this.props.match.params.id)
-      .then(albums => this.setState({ albums }));
+    return albumsGetter(this.props.match.params.id, this.state.currentPage)
+      .then(albums => this.setState({ albums, totalPages: Math.ceil(albums['release-count'] / 10) }));
+  }
+
+  changePage = destination => {
+    let currentPage = destination;
+    if(destination < 0) {
+      currentPage = 0;
+    }
+    if(destination >= this.state.totalPages) {
+      currentPage = this.state.totalPages - 1;
+    }
+    this.setState({ currentPage }, () => {
+      return albumsGetter(this.props.match.params.id, this.state.currentPage)
+        .then(res => {
+          console.log(res);
+          this.setState({ albums: res, totalPages: Math.ceil(res['release-count'] / 10) });
+        });
+    });
   }
 
   render() {
@@ -29,6 +49,7 @@ export default class ArtistView extends Component {
       <>
         <h2>{this.props.match.params.artist}</h2>
         {this.state.albums && <AlbumList albumArr={this.state.albums.releases} artist={this.props.match.params.artist} />}
+        <Paging currentPage={this.state.currentPage} totalPages={this.state.totalPages} changePage={this.changePage} />
       </>
     );
   }
--- src/services/albumsGetter.js
+++ src/services/albumsGetter.js
@@ -1,5 +1,7 @@
-export default function albumsGetter(id) {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&fmt=json`)
+export default function albumsGetter(id, currentPage) {
+  const offset = currentPage * 10;
+
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&limit=10&offset=${offset}&fmt=json`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch';
