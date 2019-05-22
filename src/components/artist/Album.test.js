9eb306a508f33ff87f5c112d3f36601067aa6688
UncleJerry <jared.myhrberg@gmail.com>
2019-05-22 09:52:55 -0700

    tests passing

--- src/components/artist/Album.test.js
+++ src/components/artist/Album.test.js
@@ -4,7 +4,16 @@ import Album from './Album';
 
 describe('Album component', () => {
   it('matches snapshot', () => {
-    const wrapper = shallow(<Album album={{}} />);
+    const album = {
+      id: '5678',
+      title: '7890',
+      date: '67890',
+      ['cover-art-archive']: {
+        front: '7800'
+      },
+    };
+    
+    const wrapper = shallow(<Album album={album} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
