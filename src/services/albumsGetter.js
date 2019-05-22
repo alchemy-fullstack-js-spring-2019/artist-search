9fa443d10409cb3a259653cf118d3cf4624dd53d
UncleJerry <jared.myhrberg@gmail.com>
2019-05-22 09:22:14 -0700

    other httpS's

--- src/services/albumsGetter.js
+++ src/services/albumsGetter.js
@@ -1,7 +1,7 @@
 export default function albumsGetter(id, currentPage) {
   const offset = currentPage * 10;
 
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${id}&limit=10&offset=${offset}&fmt=json`)
+  return fetch(`https://musicbrainz.org/ws/2/release?artist=${id}&limit=10&offset=${offset}&fmt=json`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'Unable to fetch';
